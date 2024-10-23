f1c0399d1cefc22292aacac8c0a0acb2c3996438
Tse Man Fung <andytse0917@gmail.com>
2024-10-23 11:24:52 +0800

    save

--- website/js/login.js
+++ website/js/login.js
@@ -1,47 +1,57 @@
-document.addEventListener('DOMContentLoaded', function() {
-const body=document.body;
-const eye=document.querySelector('.fa-regular');
-const beam=document.querySelector('.beam');
-const passwordInput=document.getElementById('password');
-const accountInput=document.getElementById('account');
-const btnLogin = document.getElementsByClassName("btn-login")[0];
+document.addEventListener('DOMContentLoaded', function () {
+  const body = document.body;
+  const eye = document.querySelector('.fa-regular');
+  const beam = document.querySelector('.beam');
+  const passwordInput = document.getElementById('password');
+  const accountInput = document.getElementById('account');
+  const btnLogin = document.getElementsByClassName("btn-login")[0];
 
-body.addEventListener('mousemove',function(e){
-    let rect=beam.getBoundingClientRect();
-    let mouseX=rect.right+(rect.width/2);
-    let mouseY=rect.top+(rect.height/2);
-    let rad=Math.atan2(mouseX-e.pageX,mouseY-e.pageY);
-    let deg=(rad*(20/Math.PI)*-1)-350;
-    body.style.setProperty('--beam-deg',deg+'deg');
-})
+  body.addEventListener('mousemove', function (e) {
+    let rect = beam.getBoundingClientRect();
+    let mouseX = rect.right + (rect.width / 2);
+    let mouseY = rect.top + (rect.height / 2);
+    let rad = Math.atan2(mouseX - e.pageX, mouseY - e.pageY);
+    let deg = (rad * (20 / Math.PI) * -1) - 350;
+    body.style.setProperty('--beam-deg', deg + 'deg');
+  })
 
-eye.addEventListener('click',function(e){
+  eye.addEventListener('click', function (e) {
     e.preventDefault();
     body.classList.toggle('show-password');
-    passwordInput.type=passwordInput.type==='password'?'text':'password';
-    eye.className='fa-regular '+(passwordInput.type==='password'?'fa-eye-slash':'fa-eye');
-    eye.style='color: '+(passwordInput.type==='password'?'':'white');
+    passwordInput.type = passwordInput.type === 'password' ? 'text' : 'password';
+    eye.className = 'fa-regular ' + (passwordInput.type === 'password' ? 'fa-eye-slash' : 'fa-eye');
+    eye.style = 'color: ' + (passwordInput.type === 'password' ? '' : 'white');
     passwordInput.focus();
-})
+  })
 
-btnLogin.addEventListener('click',function(e){
-    e.preventDefault();
+  btnLogin.addEventListener('click', function (e) {
+    console.log("click");
+    //e.preventDefault();
     account = $("#account").val(accountInput.value);
     password = $("#password").val(passwordInput.value);
-    if(account===''||password===''){
-        alert('Please fill in all fields');
-        return;
+    if (account === '' || password === '') {
+      $("#main-modal-primary").hide();
+      $("#main-modal-title").text("login failed");
+      $("#main-modal-body").text('Please fill in all fields');
+      $("#main-modal").show();
+      return;
     }
-    if(password==="Ab12345678"){
-      if(account==="C"){
+    if (password === "Ab12345678") {
+      if (account === "C") {
         window.location.href = "./customer/index.html";
       }
-      else if(account==="V"){
+      else if (account === "V") {
         window.location.href = "./vehicle/index.html";
       }
-      else if(account==="I"){
+      else if (account === "I") {
         window.location.href = "./insurance/index.html";
       }
     }
-});
+    else {
+      $("#main-modal-primary").hide();
+      $("#main-modal-title").text("login failed");
+      $("#main-modal-body").text('Incorrect username or password');
+      $("#main-modal").show();
+    }
+  });
 })
