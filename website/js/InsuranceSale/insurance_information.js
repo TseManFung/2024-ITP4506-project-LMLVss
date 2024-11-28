c3f331544e16b6aef845e03435e1980594910bf2
TseManFung <163531908+TseManFung@users.noreply.github.com>
2024-11-28 23:18:58 +0800

    調整樣式和功能，新增載入動畫，更新報價輸入步驟

--- website/js/InsuranceSale/insurance_information.js
+++ website/js/InsuranceSale/insurance_information.js
@@ -10,11 +10,47 @@ $(document).ready(() => {
     const curr = parseInt(btn_next.attr("curr"));
     const fin = btn_next.attr("fin");
     if (curr == fin) {
-        //TODO: 報價
+      modelEnableStatic();
+      modalSetTitle("insurance quotation");
+      modalLoadBody("./insurance_quotation.html");
+      modalSetCloseBtn(false);
+      modalSetSecondaryBtn(true, "Back");
+      modalSetPrimaryBtn(true, "Confirm", () => {
+        i = checkInputIn("main-modal-body", "is-invalid");
+        if (i.length > 0) {
+          return;
+        }
+        modalSetPrimaryBtn(false);
+        modalSetSecondaryBtn(false);
+        modalSetTitle("Processing");
+        modalLoadBody("../component/loader.html");
+        setTimeout(() => {
+          modalSetTitle("Successful");
+          modalSetBody(
+            "The page will be re-directed in three seconds. Please wait a moment..."
+          );
+          setTimeout(() => {
+            window.location.href = "./application_manage.html";
+          }, 3000);
+        }, 2000);
+      });
+      showModal();
     } else {
       $(`#step-label-${curr + 1}`).click();
     }
   });
+
+  $("#btnReject").click(() => {
+    modalDisableStatic();
+    modalSetTitle("Reject");
+    modalSetBody("Are you sure to reject the application?");
+    modalSetCloseBtn(true);
+    modalSetPrimaryBtn(true, "Confirm", () => {
+      window.location.href = "./application_manage.html";
+    });
+    showModal();
+  });
+
   $("#CarBrand").on("change", (e) => {
     const brand = $("#CarBrand").val();
     const CarModal = $("#CarModal");
