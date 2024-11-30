df233567eff512005a4e16c4c04a3856c32805bc
TseManFung <163531908+TseManFung@users.noreply.github.com>
2024-12-01 02:08:33 +0800

    新增比較保險計畫的功能，更新報價輸入預設值，優化按鈕樣式及行為，並新增比較頁面

--- website/js/insurance/insurance_detail.js
+++ website/js/insurance/insurance_detail.js
@@ -6,4 +6,8 @@ $(document).ready(() => {
     $("#apply").on("click", () => {
         window.location.href = "../login.html?redirect=insurance/input_information.html";
     });
+
+    $("#btn-compare").click(() => {
+        window.location.href = "./compare.html";
+    })
 });
